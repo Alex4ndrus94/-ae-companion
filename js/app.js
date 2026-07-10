82c931cc56dbe9670987931a758d47c31af81492
Alexandrus94 <alessandrochindemi94@gmail.com>
2026-07-11 01:32:26 +0200

    Create app.js and connect it to index.html

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const player = {
+    name: "Alexandrus94",
+    parcels: 135,
+    commons: 66,
+    rares: 38,
+    epics: 25,
+    legendaries: 6,
+    badges: 6,
+    boost: "x10",
+    mayorTarget: 217,
+    mayorCurrent: 217,
+    totalRent: 12.77
+};
+
+console.log("AE Companion avviato!");
+console.log(player);
