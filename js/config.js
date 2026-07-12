9021f5e6ec0dcce4cb7fdda06c5867984755caf3
Alexandrus94 <alessandrochindemi94@gmail.com>
2026-07-12 17:45:28 +0200

    Create Italy configuration

---
+++ js/config.js
@@ -0,0 +1,28 @@
+// ======================================
+// AE Companion Configuration - Italia
+// Versione 0.1
+// ======================================
+
+const CONFIG = {
+
+    country: "IT",
+
+    // Breakpoint Boost (Italia)
+    breakpoints: [
+        {
+            min: 0,
+            max: 149,
+            boost: 20
+        },
+        {
+            min: 150,
+            max: 99999,
+            boost: 15
+        }
+    ],
+
+    landCostAB: 100,
+
+    badgeCostAB: 200
+
+};
