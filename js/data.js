70ac2651ec57e18f3e7f2da507ed0c2a2dffced2
Alexandrus94 <alessandrochindemi94@gmail.com>
2026-07-14 23:37:26 +0200

    Add data.js to js folder

---
+++ js/data.js
@@ -0,0 +1,26 @@
+// ======================================
+// AE Companion - Player Data
+// ======================================
+
+const player = {
+
+    name: "Alexandrus94",
+
+    lands: {
+
+        common: 66,
+        rare: 38,
+        epic: 25,
+        legendary: 6
+
+    },
+
+    badges: 6,
+
+    mayorTarget: 217,
+
+    dailyAB: 91,
+
+    explorerPass: true
+
+};
