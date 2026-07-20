13fe870b82c8ac1b7ca6f07605798f8de8601929
Alexandrus94 <alessandrochindemi94@gmail.com>
2026-07-20 12:26:15 +0200

    refactor(core): introduce player.js profile structure

---
+++ js/js/player.js
@@ -0,0 +1,26 @@
+// ======================================
+// AE Companion - Player Data
+// ======================================
+
+const player = {
+
+    name: "Alexandrus94",
+
+    lands: {
+
+        common: 73,
+        rare: 45,
+        epic: 27,
+        legendary: 6
+
+    },
+
+    badges: 6,
+
+    mayorTarget: 217,
+
+    dailyAB: 91,
+
+    explorerPass: true
+
+};
